26d6a7425a0105bcd603eb70de81ed90bc3b80a8
Gabriel <62269094+gfleite@users.noreply.github.com>
2024-11-22 17:42:20 -0300

    Add the solution into the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray, ...args) {
+    const newArray = [];
+
+    oldArray.forEach(element => {
+        if(!args.includes(element)){
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
